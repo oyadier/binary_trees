c5f0efa06e5c30d168bee47950ca7d7db9dcbd8a
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2024-01-06 05:48:36 +0000

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
+	new_node = malloc(sizeof(binary_tree_t) * 2);
 
 	if (!new_node || !parent)
 		return (NULL);
