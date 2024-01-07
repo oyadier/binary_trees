4f9b5f856da7b2818a7d453bb26352ef327833e4
oyadier robert mensah <oyadierrobertmensah@gmail.com>
2024-01-07 08:29:17 +0000

    Preorder

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,7 +6,7 @@
  * @tree: a pointer to the root node of the tree to traverse
  * @func: a pointer to a function to a call ofr each node.
  *	The value in the node must be passed as a parameter to this function.
- * 
+ *
  * Return: Nothing
  */
 
@@ -18,6 +18,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	func(tree->n);
-	binary_tree_preorder(tree->left,func);
-	binary_tree_preorder(tree->right,func);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
